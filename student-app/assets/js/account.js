ceb29b267f5f6fe95f885f46ca3f932d5fefe116
Adeeshnoor <adeeshnoor.singh@gmail.com>
2018-03-24 21:00:16 +0530

    some minor changes and js files being written

---
+++ student-app/assets/js/account.js
@@ -0,0 +1,66 @@
+var  regno = document.getElementById("regno")
+
+var roll = document.getElementById("rollno")
+
+var student_name = document.getElementById("stuname")
+
+var uni_rank = document.getElementById("rank")
+
+var dob = document.getElementById("dob")
+
+var category = document.getElementById("category")
+
+var gender = document.getElementById("gender")
+
+
+var father_name = document.getElementById("father_name")
+
+var mother_name = document.getElementById("mother_name")
+
+var address = document.getElementById("address")
+
+var mobile_no = document.getElementById("mobile_no")
+
+var email_id = document.getElementById("email_id")
+
+
+
+
+
+var board_10 = document.getElementById("board10")
+
+var passingYear_10 = document.getElementById("pass10")
+
+var marks_10 = document.getElementById("marks10")
+
+var percentage_10 = document.getElementById("percent10")
+
+var division_10 = document.getElementById("division10")
+
+
+
+var board_12 = document.getElementById("board12")
+
+var passingYear_12 = document.getElementById("pass12")
+
+var marks_12 = document.getElementById("maeks12")
+
+var percentage_12 = document.getElementById("percent12")
+
+var division_12 = document.getElementById("division12")
+
+
+
+var board_grad =document.getElementById("boardgrad")
+
+var passingYear_grad = document.getElementById("passgrad")
+
+var marks_grad = document.getElementById("marksgrad")
+
+var percentage_grad = document.getElementById("percentgrad")
+
+var division_grad = document.getElementById("divisiongrad")
+
+
+
+
