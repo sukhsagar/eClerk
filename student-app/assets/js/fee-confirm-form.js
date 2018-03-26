b7bca8ee043c96fb051f72ba84bb057954e6e23d
Adeeshnoor <adeeshnoor.singh@gmail.com>
2018-03-26 22:23:27 +0530

    js files added to student-app in the project

---
+++ student-app/assets/js/fee-confirm-form.js
@@ -0,0 +1,11 @@
+var studentName = document.getElementById("stuname")
+
+var rollNo = document.getElementById("rollno")
+
+var stuClass = document.getElementById("class")
+
+var depositID = document.getElementById("deposit_ID")
+
+var journalID = document.getElementById("Journal_ID")
+
+var Deposit_Date = document.getElementById("depositDate")
---
+++ student-app/assets/js/scholarship-form.js
@@ -0,0 +1,15 @@
+var stuName = document.getElementById("stuname")
+
+var rollNo = document.getElementById("rollno")
+
+var stuClass = document.getElementById("class")
+
+var category = document.getElementById("category")
+
+var minority = document.getElementById("minority")
+
+var incomeCertificate = document.getElementById("incomeCertificate")
+
+var residenceCertificate = document.getElementById("residenceCertificate")
+
+var concession = document.getElementById("concession")
