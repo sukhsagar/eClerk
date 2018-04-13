b0e9be196fe355780de78d0eac85ae0d7d48502c
Adeeshnoor <adeeshnoor.singh@gmail.com>
2018-04-13 11:04:38 +0530

    validations added to files

--- student-app/assets/js/jashan-form.js
+++ student-app/assets/js/jashan-form.js
@@ -1,8 +1,26 @@
-var studentName = document.getElementById("stuname")
+function validate(){
+    var studentName = document.getElementById("stuname")
+    var rollNo = document.getElementById("rollno")
+    var stuClass = document.getElementById("class")
+    var event1 = document.getElementById("event1")
+    var event2 = document.getElementById("event2")
+    var event3 = document.getElementById("event3")
+    var event4 = document.getElementById("event4")
+    var event5 = document.getElementById("event5")
 
-var rollNo = document.getElementById("rollno")
+    if(studentName.value===""){
+        alert("Enter your Name"); return;
+    }
+    if(rollNo.value===""){
+        alert("Enter your Roll No"); return;
+    }
+    if(stuClass.value===""){
+        alert("Enter your Class"); return;
+    }
+    if(event1.checked==false || event2.checked==false || event3.checked==false || event4.checked==false || event5.checked==false){
+        alert("select atleast 1 event"); return;
+    }
+}
 
-var stuClass = document.getElementById("class")
 
-var event = document.getElementById("Event")
 
