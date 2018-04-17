b7ff73f036260a9ddbd1e64d86f6a30910508e96
Gagan <gagusingh1117@gmail.com>
2018-04-17 14:17:35 +0530

    added javascript code all admin panel forms

--- admin-panel/assets/js/examination.js
+++ admin-panel/assets/js/examination.js
@@ -1,23 +1,124 @@
-var registrationno = document.getElementById("iregistrationno")
-var rollno = document.getElementById("irollno")
-var name = document.getElementById("iname")
-var gender = document.getElementById("igender")
-var category = document.getElementById("icategory")
-var fathername = document.getElementById("ifathername")
-var mothername = document.getElementById("imothername")
-var address = document.getElementById("iaddress")
-var mobileno = document.getElementById("imobileno")
-var city = document.getElementById("icity")
-var medium = document.getElementById("imedium")
-var examcenter = document.getElementById("iexamcenter")
-var examcode = document.getElementById("iexamcode")
-var universityboard = document.getElementById("iuniversityboard")
-var passingyear = document.getElementById("ipassingyear")
-var marks = document.getElementById("imarks")
-var percentage = document.getElementById("ipercentage")
-var division = document.getElementById("idivision")
-var subjectname = document.getElementById("isubjectname")
-var subjectcode = document.getElementById("isubjectcode")
-var date = document.getElementById("idate")
-var place = document.getElementById("iplace")
-var studentsign = document.getElementById("istudentsign")
+function validate() {
+    var registrationno = document.getElementById("iregistrationno");
+    var rollno = document.getElementById("irollno");
+    var name = document.getElementById("iname");
+    var gender = document.getElementById("igender");
+    var category = document.getElementById("icategory");
+    var fathername = document.getElementById("ifathername");
+    var mothername = document.getElementById("imothername");
+    var address = document.getElementById("iaddress");
+    var mobileno = document.getElementById("imobileno");
+    var city = document.getElementById("icity");
+    var medium = document.getElementById("imedium");
+    var examcenter = document.getElementById("iexamcenter");
+    var examcode = document.getElementById("iexamcode");
+    var universityboard = document.getElementById("iuniversityboard");
+    var passingyear = document.getElementById("ipassingyear");
+    var marks = document.getElementById("imarks");
+    var percentage = document.getElementById("ipercentage");
+    var division = document.getElementById("idivision");
+    var subjectname = document.getElementById("isubjectname");
+    var subjectcode = document.getElementById("isubjectcode");
+    var date = document.getElementById("idate");
+    var place = document.getElementById("iplace");
+    var studentsign = document.getElementById("istudentsign");
+    var submitbutton = document.getElementById("isubmit");
+    var canclebutton = document.getElementById("icancle");
+
+
+    if(registrationno.value==="")
+    {
+        alert("please enter registration no");
+        return;
+    }
+    if(rollno.value==="")
+    {
+        alert("please enter rollno");
+        return;
+    }
+    if(name.value==="")
+    {
+        alert("please enter name");
+        return;
+    }
+    if(fathername.value==="")
+    {
+        alert("please enter fathername");
+        return;
+    }
+    if(mothername.value==="")
+    {
+        alert("please enter mothername");
+        return;
+    }
+    if(address.value==="")
+    {
+        alert("please enter address");
+        return;
+    }
+    if(mobileno.value==="")
+    {
+        alert("please enter mobile no");
+        return;
+    }
+    if(examcenter.value==="")
+    {
+        alert("please enter examcenter");
+        return;
+    }
+    if(examcode.value==="")
+    {
+        alert("please enter examcode");
+        return;
+    }
+    if(universityboard.value==="")
+    {
+        alert("please enter university board");
+        return;
+    }
+    if(passingyear.value==="")
+    {
+        alert("please enter passingyear");
+        return;
+    }
+    if(marks.value==="")
+    {
+        alert("please enter marks");
+        return;
+    }
+    if(percentage.value==="")
+    {
+        alert("please enter percentage");
+        return;
+    }
+    if(division.value==="")
+    {
+        alert("please enter division");
+        return;
+    }
+    if(subjectcode.value==="")
+    {
+        alert("please enter subjectcode");
+        return;
+    }
+    if(subjectname.value==="")
+    {
+        alert("please enter subjectname");
+        return;
+    }
+    if(date.value==="")
+    {
+        alert("please enter date");
+        return;
+    }
+    if(place.value==="")
+    {
+        alert("please enter place");
+        return;
+    }
+    if(studentsign.value==="")
+    {
+        alert("please enter signature");
+        return;
+    }
+}
--- admin-panel/assets/js/sports.js
+++ admin-panel/assets/js/sports.js
@@ -1,10 +1,56 @@
-var name = document.getElementById("iname")
-var rollno = document.getElementById("irollno")
-var spclass = document.getElementById("iclass")
-var dateofbirth = document.getElementById("idob")
-var gender = document.getElementById("igender")
-var mobileno = document.getElementById("imobileno")
-var emailid = document.getElementById("iemailid")
-var selectgames = document.getElementById("iselectgames")
-var submitbutton = document.getElementById("isubmit")
-var resetbutton = document.getElementById("ireset")
+function validate() {
+    var name = document.getElementById("iname");
+    var rollno = document.getElementById("irollno");
+    var spclass = document.getElementById("iclass");
+    var dateofbirth = document.getElementById("idob");
+    var gender = document.getElementById("igender");
+    var mobileno = document.getElementById("imobileno");
+    var emailid = document.getElementById("iemailid");
+    var selectgames1 = document.getElementById("i1selectgames");
+    var selectgames2 = document.getElementById("i2selectgames");
+    var selectgames3 = document.getElementById("i3selectgames");
+    var selectgames4 = document.getElementById("i4selectgames");
+    var selectgames5 = document.getElementById("i5selectgames");
+    var selectgames6 = document.getElementById("i6selectgames");
+    var selectgames7 = document.getElementById("i7selectgames");
+    var selectgames8 = document.getElementById("i8selectgames");
+    var selectgames9 = document.getElementById("i9selectgames");
+    var submitbutton = document.getElementById("isubmit");
+    var canclebutton = document.getElementById("icancle");
+
+    if(name.value==="")
+    {
+        alert("please enter name");
+        return;
+    }
+    if(rollno.value==="")
+    {
+        alert("please enter rollno");
+        return;
+    }
+    if(spclass.value==="")
+    {
+        alert("please enter class");
+        return;
+    }
+    if(dateofbirth.value==="")
+    {
+        alert("please enter date of birth");
+        return;
+    }
+    if(mobileno.value==="")
+    {
+        alert("please enter mobile no");
+        return;
+    }
+    if(emailid.value==="")
+    {
+        alert("please enter email id");
+        return;
+    }
+    if(selectgames1.checked==false && selectgames2.checked==false && selectgames3.checked==false && selectgames4.checked==false && selectgames5.checked==false && selectgames6.checked==false && selectgames7.checked==false && selectgames8.checked==false && selectgames9.checked==false)
+    {
+        alert("Please select atleast 1 game");
+        return;
+    }
+}
