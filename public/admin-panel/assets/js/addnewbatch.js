34b7a40933b0b96363bcaec834be25523321cb65
Gagan <gagusingh1117@gmail.com>
2018-05-10 18:58:13 +0530

    add js validations code in addnewbatch page

--- public/admin-panel/assets/js/addnewbatch.js
+++ public/admin-panel/assets/js/addnewbatch.js
@@ -1,4 +1,5 @@
 function validate(){
+    var batch = document.getElementById("ibatch");
     var name = document.getElementById("iname");
     var fathername = document.getElementById("ifathername");
     var mothername = document.getElementById("imothername");
@@ -7,12 +8,43 @@ function validate(){
     var registrationno = document.getElementById("iregistrationno");
     var submitbutton = document.getElementById("isubmit");
     var stuClass = document.getElementById("inputState");
+    
+    if(name.value==="")
+    {
+        alert("please enter name");
+        return;
+    }
+    if(fathername.value==="")
+    {
+        alert("please enter fathername");
+        return;
+    }
+    if(mothername.value==="")
+    {
+        alert("please enter mothername");
+        return;
+    }
+    if(rollno.value==="")
+    {
+        alert("please enter rollno");
+        return;
+    }
+    if(password.value==="")
+    {
+        alert("please enter password");
+        return;
+    }
+    if(registrationno.value==="")
+    {
+        alert("please enter registration no");
+        return;
+    }
     var formData={
         "name": name.value,
         "rollNo": rollno.value,
         "password": password.value,
         "registrationNumber": registrationno.value,
-        "class": stuClass.selectedIndex,
+        "batch": batch.value,
         "personalDetail":{
             "fatherName": fathername.value,
             "motherName": mothername.value
