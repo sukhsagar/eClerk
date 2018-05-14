d71acdfe8ccd65591376f5a3618c99d3ed401222
Gagan <gagusingh1117@gmail.com>
2018-05-15 01:00:34 +0530

    added two textbox in addfaculity form

--- public/admin-panel/assets/js/addfaculty.js
+++ public/admin-panel/assets/js/addfaculty.js
@@ -1,14 +1,44 @@
 function validate(){
-    var teacherName = document.getElementById("iteachername");
-    var teacherType = document.getElementById("iteachertype");
-    var uploadPicture = document.getElementById("iuploadpicture");
+    var teacherName = document.getElementById("iteacherName");
+    var teacherType = document.getElementById("iteacherType");
+    var uploadPicture = document.getElementById("iuploadPicture");
+    var recentWork = document.getElementById("irecentWork");
+    var message = document.getElementById("imessage");
     var submitButton = document.getElementById("isubmit");
     var cancleButton = document.getElementById("icancle");
+
+    if(teacherName.value==="")
+    {
+        alert("please enter teacher name");
+        return;
+    }
+    if(teacherType.value==="")
+    {
+        alert("please enter teacher type");
+        return;
+    }
+    if(uploadPicture.value==="")
+    {
+        alert("please upload teacher image");
+        return;
+    }
+    if(recentWork.value==="")
+    {
+        alert("please enter teacher recent work");
+        return;
+    }
+    if(message.value==="")
+    {
+        alert("please enter teacher message");
+        return;
+    }
 
     var formData={
         "teacherName" : teacherName.value,
         "teacherType" : teacherType.value,
-        "uploadPicture" : uploadPicture.value
+        "uploadPicture" : uploadPicture.value,
+        "recentWork" : recentWork.value,
+        "message" : message.value
     };
     firebase.database().ref('faculity/' + teacherName.value).push(formData).then(function(){
         alert("You have succesfully added teacher detail.");
