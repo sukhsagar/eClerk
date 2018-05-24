5ac6b2d11761c289931ee260c980174f67648cfe
Gagan <gagusingh1117@gmail.com>
2018-05-24 21:32:48 +0530

    added js code in admin panel home page

--- public/admin-panel/assets/js/home.js
+++ public/admin-panel/assets/js/home.js
@@ -1,3 +1,7 @@
+function load(){
+    firebase.database().ref('gndu-amritsar/timeTable/').once('value',function(snapshot1){
+        data=snapshot1.val();
+
 var mon1 = document.getElementById("mon1");
 var mon2 = document.getElementById("mon2");
 var mon3 = document.getElementById("mon3");
@@ -38,9 +42,65 @@ var fri5 = document.getElementById("fri5");
 var fri6 = document.getElementById("fri6");
 var fri7 = document.getElementById("fri7");
 var fri8 = document.getElementById("fri8");
+
+        mon1.innerHTML = data.monday.first;
+        mon2.innerHTML = data.monday.second;
+        mon3.innerHTML = data.monday.third;
+        mon4.innerHTML = data.monday.fourth;
+        mon5.innerHTML = data.monday.fifth;
+        mon6.innerHTML = data.monday.sixth;
+        mon7.innerHTML = data.monday.seventh;
+        mon8.innerHTML = data.monday.eighth;
+
+        tues1.innerHTML = data.monday.first;
+        tues2.innerHTML = data.monday.second;
+        tues3.innerHTML = data.monday.third;
+        tues4.innerHTML = data.monday.fourth;
+        tues5.innerHTML = data.monday.fifth;
+        tues6.innerHTML = data.monday.sixth;
+        tues7.innerHTML = data.monday.seventh;
+        tues8.innerHTML = data.monday.eighth;
+
+        wed1.innerHTML = data.monday.first;
+        wed2.innerHTML = data.monday.second;
+        wed3.innerHTML = data.monday.third;
+        wed4.innerHTML = data.monday.fourth;
+        wed5.innerHTML = data.monday.fifth;
+        wed6.innerHTML = data.monday.sixth;
+        wed7.innerHTML = data.monday.seventh;
+        wed8.innerHTML = data.monday.eighth;
+
+        thu1.innerHTML = data.monday.first;
+        thu2.innerHTML = data.monday.second;
+        thu3.innerHTML = data.monday.third;
+        thu4.innerHTML = data.monday.fourth;
+        thu5.innerHTML = data.monday.fifth;
+        thu6.innerHTML = data.monday.sixth;
+        thu7.innerHTML = data.monday.seventh;
+        thu8.innerHTML = data.monday.eighth;
+
+        fri1.innerHTML = data.monday.first;
+        fri2.innerHTML = data.monday.second;
+        fri3.innerHTML = data.monday.third;
+        fri4.innerHTML = data.monday.fourth;
+        fri5.innerHTML = data.monday.fifth;
+        fri6.innerHTML = data.monday.sixth;
+        fri7.innerHTML = data.monday.seventh;
+        fri8.innerHTML = data.monday.eighth;
+})
 var notice1 = document.getElementById("notice1");
 var notice2 = document.getElementById("notice2");
 var notice3 = document.getElementById("notice3");
+
+firebase.database().ref('gndu-amritsar/teacherOnLeave/').once('value',function(snapshot2){
+    data=snapshot2.val();
+
 var teacherLeave1 = document.getElementById("teacherLeave1");
 var teacherLeave2 = document.getElementById("teacherLeave2");
 var teacherLeave3 = document.getElementById("teacherLeave3");
+
+            teacherLeave1.innerHTML = data.teacherName;
+            teacherLeave2.innerHTML = data.teacherName;
+            teacherLeave3.innerHTML = data.teacherName;
+})
+}
