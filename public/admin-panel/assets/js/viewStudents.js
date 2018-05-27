2d0d537d351fcef18d7032d9fa26dfb028f4577a
rajatsharma160293 <rajat@nobbas.com>
2018-05-28 02:28:26 +0530

    populated data

--- public/admin-panel/assets/js/viewStudents.js
+++ public/admin-panel/assets/js/viewStudents.js
@@ -1,14 +1,19 @@
 function loadData(){
     var students = {};
     firebase.database().ref('gndu-amritsar/classStudents/MCA-(TYC)/').once('value',function(snapshot){
-        students=snapshot.val();
+		students=snapshot.val();
     }).then(function(){
-    	//Code check krli.
-    	foreach(students)
-    	firebase.database().ref('gndu-amritsar/student/'+rollNo).once('value',function(snapshot){
-	        studentDetail=snapshot.val();
-	    }).then(function(){
-	    	//Ethe student d row gennerate krdo.
+		students.forEach(rollNo => {
+			firebase.database().ref('gndu-amritsar/student/' + rollNo).once('value', function (snapshot) {
+				studentDetail = snapshot.val();
+			}).then(function () {
+				let row = "<tr><td><label>" + studentDetail.name + "</label></td><td><label>" + studentDetail.personalDetail.fatherName 
+					+ "</label></td><td><label>" + studentDetail.personalDetail.motherName + "</label></td><td><label>" + studentDetail.rollNo 
+					+ "</label></td><td><label>" + studentDetail.registrationNumber + "</label></td></tr>";
+				document.getElementById('studentDetailContainer').innerHTML += row;
+			});
+		}); 
+	});
+}
 
-	    });
-    });
+loadData();
