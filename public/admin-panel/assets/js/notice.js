7f7a8bebf623d95329d0061341760e83f3054da1
Adeeshnoor <adeeshnoor.singh@gmail.com>
2018-05-29 09:42:56 +0530

    new commit

--- public/admin-panel/assets/js/notice.js
+++
@@ -1,7 +0,0 @@
-var title = document.getElementById("ititle")
-var type = document.getElementById("itype")
-var detail = document.getElementById("idetail")
-var uploaded = document.getElementById("iuploaded")
-var validity = document.getElementById("ivalidity")
-var editButton = document.getElementById("iedit")
-var viewallButton = document.getElementById("iviewall")
--- public/readme.md
+++
@@ -1,2 +0,0 @@
-eClerk
-This project aims at digitally gathering the information of the students of the computer science ddedpartment - GNDU.
